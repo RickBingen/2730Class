76c378c169e684006e91acef1fa4f91d3ff6baad
RickBingen <51863040+RickBingen@users.noreply.github.com>
2024-02-27 11:01:12 -0600

    Add Gun  Fire Animation

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -9,8 +9,17 @@ public class Gun : MonoBehaviour
     [SerializeField] Transform _bulletSpawnPoint;
     [SerializeField] Bullet _bulletPrefab;
 
+    static readonly int FIRE_HASH = Animator.StringToHash("Fire");
     Vector2 _mousePos;
-    
+
+    Animator _animator;
+
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         Shoot();
@@ -20,11 +29,13 @@ public class Gun : MonoBehaviour
     void OnEnable()
     {
         OnShoot += ShootProjectile;
+        OnShoot += FireAnimation;
     }
 
     void OnDisable()
     {
         OnShoot -= ShootProjectile;
+        OnShoot -= FireAnimation;
     }
 
     void Shoot()
@@ -39,6 +50,11 @@ public class Gun : MonoBehaviour
     {
         Bullet newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
         newBullet.Init(_bulletSpawnPoint.position, _mousePos);
+    }
+
+    void FireAnimation()
+    {
+        _animator.Play(FIRE_HASH, 0,0f);
     }
 
     void RotateGun()
